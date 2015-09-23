77d6af00a6b390637a2ee18061b8121d477e56bf
Josafá Filho <josafafilho15@gmail.com>
2015-09-23 16:21:31 -0300

    Adiciona página de configurações do plugin WP Side Comments Ajusta inicializadores das classes do plugin para serem executados a partir do arquivo de cada classe

--- wp-side-comments.php
+++ wp-side-comments.php
@@ -16,41 +16,18 @@ if (!defined('ABSPATH')) {
 define('CTLT_WP_SIDE_COMMENTS_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
-//includes side wp comments class
+/**
+ * Each plugin's class included below is responsible for register its own hooks and initializers
+ */
+
+//includes wp side comments class
 require_once CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH . 'classes/class-wp-side-comments.php';
 
-//includes required classes for comment voting
-require_once CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH . 'classes/class-visitor.php';
+//includes wp side comments admin class
+require_once CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH . 'classes/class-wp-side-comments-admin.php';
 
-//Include the Custom Post Type "Texto em Debate"
+//includes the Custom Post Type "Texto em Debate"
 require_once CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH . 'classes/class-texto-em-debate-post-type.php';
 
 // Widget para exibição na capa
 include(CTLT_WP_SIDE_COMMENTS_PLUGIN_PATH . 'classes/comment-front-widget.php');
-
-function wpsc_init_side_comments()
-{
-    global $CTLT_WP_Side_Comments;
-    $CTLT_WP_Side_Comments = new CTLT_WP_Side_Comments();
-
-    //TODO: vamos bloquear os votos de usuários não logados?
-    if (is_user_logged_in()) {
-        $visitor = new WP_Side_Comments_Visitor_Member(get_current_user_id());
-    } else {
-        $visitor = new WP_Side_Comments_Visitor_Guest($_SERVER['REMOTE_ADDR']);
-    }
-
-    if (!($CTLT_WP_Side_Comments->getVisitor() instanceof WP_Side_Comments_Visitor)) {
-        $CTLT_WP_Side_Comments->setVisitor($visitor);
-    }
-}
-
-add_action('plugins_loaded', 'wpsc_init_side_comments');
-
-function wpsc_init_texto_em_debate()
-{
-    global $Texto_Em_Debate_Post_Type;
-    $Texto_Em_Debate_Post_Type = new Texto_Em_Debate_Post_Type();
-}
-
-add_action('init', 'wpsc_init_texto_em_debate');
